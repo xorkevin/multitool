1c1492ce0558d9c18e186429648fc66d77deb25d
Kevin Wang <kevin@xorkevin.com>
2025-04-27 03:24:55 -0700

    Bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "dev.xorkevin.multitool"
         minSdk = 35
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
