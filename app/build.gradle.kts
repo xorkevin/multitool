44f48d4cb2156a0431e136c1ae5c0cd7627c2329
Kevin Wang <kevin@xorkevin.com>
2025-11-03 15:15:15 -0800

    Fix minify build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,16 +17,15 @@ android {
         applicationId = "dev.xorkevin.multitool"
         minSdk = 33
         targetSdk = 36
-        versionCode = 3
-        versionName = "2.0"
+        versionCode = 4
+        versionName = "2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
         release {
-            isMinifyEnabled = true
-            isShrinkResources = true
+            isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro"
             )
